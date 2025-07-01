9dd8fff6aa7e04a6d86a121591f6c1e7d6c29267
Monish Nallagondalla <nsmonish@gmail.com>
2025-07-01 23:56:45 +0530

    Data ingestion start

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,3 +6,5 @@ from src.logger import get_logger
 from src.custom_exception import CustomException
 from config.paths_config import *
 from utils.common_functions import read_yaml
+
+logger = get_logger(__name__)
